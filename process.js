736ebdcb7c7c476c9e80d67800115afee9fe1c4d
Subhajit Sahu <wolfram77@gmail.com>
2023-11-13 22:36:56 +0530

    :bug: process output logs

---
+++ process.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const ROMPTH = /^OMP_NUM_THREADS=(\d+)/m;
+const RRESLT = /^\{run=(.+?), thread=(.+?), node=(.+?), core=(.+?), time=(.+?)ms, flops=(.+?)\}/m;
+
+
+
+
+// *-FILE
+// ------
+
+function readFile(pth) {
+  var d = fs.readFileSync(pth, 'utf8');
+  return d.replace(/\r?\n/g, '\n');
+}
+
+function writeFile(pth, d) {
+  d = d.replace(/\r?\n/g, os.EOL);
+  fs.writeFileSync(pth, d);
+}
+
+
+
+
+// *-CSV
+// -----
+
+function writeCsv(pth, rows) {
+  var cols = Object.keys(rows[0]);
+  var a = cols.join()+'\n';
+  for (var r of rows)
+    a += [...Object.values(r)].map(v => `"${v}"`).join()+'\n';
+  writeFile(pth, a);
+}
+
+
+
+
+// *-LOG
+// -----
+
+function readLogLine(ln, data, state) {
+  ln = ln.replace(/^\d+-\d+-\d+ \d+:\d+:\d+\s+/, '');
+  if (ROMPTH.test(ln)) {
+    var [, omp_num_threads] = ROMPTH.exec(ln);
+    state.omp_num_threads   = parseFloat(omp_num_threads);
+    if (!data.has(state.graph || '')) data.set(state.graph || '', []);
+  }
+  else if (RRESLT.test(ln)) {
+    var [, run, thread, node, core, time, flops] = RRESLT.exec(ln);
+    data.get(state.graph || '').push(Object.assign({}, state, {
+      run:    parseFloat(run),
+      thread: parseFloat(thread),
+      node:   parseFloat(node),
+      core:   parseFloat(core),
+      time:   parseFloat(time),
+      flops:  parseFloat(flops),
+    }));
+  }
+  return state;
+}
+
+function readLog(pth) {
+  var text  = readFile(pth);
+  var lines = text.split('\n');
+  var data  = new Map();
+  var state = {};
+  for (var ln of lines)
+    state = readLogLine(ln, data, state);
+  return data;
+}
+
+
+
+
+// PROCESS-*
+// ---------
+
+function processCsv(data) {
+  var a = [];
+  for (var rows of data.values())
+    a.push(...rows);
+  return a;
+}
+
+
+
+
+// MAIN
+// ----
+
+function main(cmd, log, out) {
+  var data = readLog(log);
+  if (path.extname(out)==='') cmd += '-dir';
+  switch (cmd) {
+    case 'csv':
+      var rows = processCsv(data);
+      writeCsv(out, rows);
+      break;
+    case 'csv-dir':
+      for (var [graph, rows] of data)
+        writeCsv(path.join(out, graph+'.csv'), rows);
+      break;
+    default:
+      console.error(`error: "${cmd}"?`);
+      break;
+  }
+}
+main(...process.argv.slice(2));
